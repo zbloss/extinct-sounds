e5ed020c8144f9b90566a33e9cf7c6492015eb5c
Zach Bloss <zacharybloss@gmail.com>
2022-10-30 12:29:49 -0400

    make Metamask dependency
    TODO: add prmopt to install metamask

--- website/src/components/LogIn.tsx
+++ website/src/components/LogIn.tsx
@@ -4,34 +4,16 @@ import SwitchNetwork from "./SwitchNetwork";
 
 const LogIn = async () => {
     
-    const url = `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`
-    console.log("url:", url);
-
-    //const provider = new ethers.providers.JsonRpcProvider(url)
-
+    
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    // const provider = new ethers.providers.InfuraProvider(
-    //     "goerli", 
-    //     "2b16ea43114d46f1aea7b677a52abd4f"
-    // )
-    console.log("provider:", provider, provider.connection)
-
     const signer = await provider.getSigner()
-    console.log("signer:", signer, signer._address)
-
     const network = await provider.getNetwork()
     console.log("network:", network);
 
     if (network['chainId'] !== ChainId()) {
-        console.log("changing network")
-        console.log("network[chainId]:", network['chainId'])
-        console.log("ChainId():", ChainId())
-
         await SwitchNetwork(ChainId())
     }
     
-    // const addresses = await provider.listAccounts()
-
     const addresses = await provider.send("eth_requestAccounts", []);
     console.log("addresses:", addresses)
 
--- website/src/constants/ChainId.tsx
+++ website/src/constants/ChainId.tsx
@@ -2,7 +2,7 @@ const ChainId = () => {
     if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
         return 5
     } else {
-        return 1
+        return 5
     }
 }
 export default ChainId;
