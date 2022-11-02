196e4f7c694ca5eba65c4055950115de2855cea7
Zach Bloss <zacharybloss@gmail.com>
2022-11-02 08:29:07 -0400

    add guess append functionality

--- website/src/components/SoundCard.tsx
+++ website/src/components/SoundCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -13,18 +13,17 @@ const SoundCard = () => {
 
     const imageUrl = "https://imgs.search.brave.com/FPHR0VnYnneIffc_M8R5GjtdlQmcBQAdGSE1gBCvCjk/rs:fit:1200:1080:1/g:ce/aHR0cHM6Ly9hNC1p/bWFnZXMubXlzcGFj/ZWNkbi5jb20vaW1h/Z2VzMDQvMTAvYjRh/NDQxYzI4NzA1NDI1/Njk0ODNiOTdiY2I2/NmFhMWYvZnVsbC5q/cGc"
 
-    const guesses: any = []
-    const [guess, setGuess] = useState<string | undefined>();
-
+    const [guesses, setGuesses] = useState([]);
+    const [guess, setGuess] = useState<string>('');
     const appendGuess = () => {
-        console.log("in appendguess")
-        if (guess) {
-            guesses.push(guess);
+        if (guesses.length <= 5) {    
+            // @ts-ignore
+            const newguesses = guesses.concat(guess)
+            setGuesses(newguesses)
+            
         }
-
-        console.log("guesses:", guesses)
-        setGuess(undefined)
     }
+
 
     return (
         <Grid
@@ -56,21 +55,42 @@ const SoundCard = () => {
                             spacing={2}
                             direction="column"
                             alignItems="center"
+                            sx={{ mb: 4 }}
                         >
                             <Grid item xs={12} sx={{ mb: 4 }}>
                                 <TextField 
                                     id="outlined-basic" 
                                     label="Guess" 
                                     variant="outlined" 
-                                    onSubmit={appendGuess}
                                     onChange={(e) => setGuess(e.target.value)}
+                                    onSubmit={(e) => {
+                                        // @ts-ignore
+                                        appendGuess();
+                                    }}
                                     onKeyDown={(e) => {
                                         if (e.key === 'Enter') {
+
+                                            // @ts-ignore
                                             appendGuess(); 
+                                            setGuess('');
                                         }
                                     }}
+                                    value={guess}
                                 />
                             </Grid>
+                            {guesses?.map((v, index) => {
+
+                                return (
+                                    <Grid item xs={12}>
+                                        <TextField 
+                                            disabled
+                                            id={`previous-guesses-${index + 1}`}
+                                            variant="filled" 
+                                            value={v}
+                                        />
+                                    </Grid>
+                                )
+                            })}
 
                         </Grid>
                     </Box>
